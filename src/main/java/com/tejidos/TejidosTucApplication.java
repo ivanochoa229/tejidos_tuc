4404e6034e99853679fa5425cef5f607a5ee3dfd
ivanochoa229 <ivanochoa229@gmail.com>
2025-03-16 23:40:34 -0300

    Iniciando el proyecto

---
+++ src/main/java/com/tejidos/TejidosTucApplication.java
@@ -0,0 +1,13 @@
+package com.tejidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TejidosTucApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TejidosTucApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tejidos/TejidosTucApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tejidos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TejidosTucApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
